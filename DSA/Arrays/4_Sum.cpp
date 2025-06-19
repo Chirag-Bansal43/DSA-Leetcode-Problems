925281283095bfdbbeec234a14977269ded89191
Chirag Bansal <bansalc12316@gmail.com>
2025-06-19 05:40:27 +0530

    Added Optimal Solution of 4 sum problem

--- DSA/Arrays/4_Sum.cpp
+++ DSA/Arrays/4_Sum.cpp
@@ -2,34 +2,64 @@
 using namespace std;
 
 /*---------------------------------------------------------------------------------------------------
-// Better solution - (using Sorting and  2 pointers Approach)
-T.C = O(n^3 + n logn) , S.C = O(1)
+// Better solution - (using HashSet)
+T.C = O(n^3 * log(no. of unique quadruplets) + n^3) = O(n^3 * log (n^3)) in average case = O(n^3 * logn)
+S.C = O(no. of unique triplets + n) ~ O(n^3) in average case
 
-// Reference for int S
-nums[i] + nums[j] + nums[l] + nums[k] = target
-S = nums[j] + nums[l] + nums[k] = target - nums[i]
-S is the required sum of triplets for the particular i...
+// hashSet.find(forth) , this is a point where mistake happens
+here if hashSet stores int i.e hashSet<int> then argument of find will also take integer 
+it is like this - hashSet<int>.find((int) forth) , if forth is long long then it will type cast forth in integer and if forth is out of range of integer then also it will type cast it and shows ajib sa behaviour
+So, if your argument of find is long long So, you should change the hashSet<int> to hashSet<long long>
+hashSet<long long>.find((long long) forth) : it will take long long argument And works fine
 */
-vector<vector<int>> Four_Sum_01(vector<int>& ques,int& target){
+vector<vector<int>> Four_Sum_02(vector<int>& ques,int& target){
+    set<vector<int>> AnsSet;
+    int n = ques.size();
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            unordered_set<long long> hashSet;
+            for(int l=j+1;l<n;l++){
+                //here ques[i] Explicitly type cast into long long to avoid integer overflow while doing addition in right hand side
+                // the long long in right hand side again implicitly converts into int and stores into forth variable
+                long long forth = target-(1LL * ques[i]+ques[j]+ques[l]); // 1LL * x (Explicit type conversion of x to long long)
+                if(hashSet.find(forth) != hashSet.end()){
+                    vector<int> quadruplet = {ques[i],ques[j],ques[l],(int)forth};
+                    sort(quadruplet.begin(),quadruplet.end());
+                    AnsSet.insert(quadruplet);
+                }
+                hashSet.insert(1LL * ques[l]);
+            }
+        }
+    }
+
+    vector<vector<int>> ans(AnsSet.begin(),AnsSet.end());
+    return ans;
+}
+
+
+/*---------------------------------------------------------------------------------------------------
+// Optimal Solution - (using Sorting and  2 pointers Approach)
+T.C = O(n^3 + n logn) , S.C = O(1)
+*/
+vector<vector<int>> Four_Sum(vector<int>& ques,int& target){
     vector<vector<int>> ans;
     sort(ques.begin(),ques.end());
     int n = ques.size();
     for(int i=0;i<n;i++){ // T.C = O(n^3)
         if(i>0 && ques[i]==ques[i-1])   continue;
-        int S = target-ques[i];
         for(int j=i+1;j<n;j++){
             if(j!=i+1 && ques[j]==ques[j-1]) continue;
             int l = j+1,k=n-1;
             while(l<k){
-                long long sum = (long long) ques[j] + ques[l] + ques[k];
-                if(sum == S){
+                long long sum = (long long) ques[i] + ques[j] + ques[l] + ques[k];
+                if(sum == target){
                     vector<int> quadruplet = {ques[i],ques[j],ques[l],ques[k]};
                     ans.push_back(quadruplet);
                     l++;k--;
                     while(l<k && ques[l]==ques[l-1]) l++;
                     while(l<k && ques[k]==ques[k+1]) k--;
                 }
-                else if(sum<S)  l++;
+                else if(sum<target)  l++;
                 else k--;
             }
         }
@@ -66,7 +96,7 @@ int main(){
     cout<<"Enter target : ";
     cin>>target;
 
-    vector<vector<int>> ans = Four_Sum_01(arr,target);
+    vector<vector<int>> ans = Four_Sum(arr,target);
 
     // Printing ans array
     printAns(ans);
